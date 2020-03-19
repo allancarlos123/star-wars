9e33f0b57394ec618837dca06f43c64b3ff81f95
Allan Carlos <allancarlosdeveloper@gmail.com>
2020-03-19 10:17:41 -0300

    ➕ Install Styled-Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './components/Header';
-import './App.css';
+
+import GlobalStyle from './styles/global';
 
 function App() {
   return (
     <>
+      <GlobalStyle />
       <Header />
     </>
   );
